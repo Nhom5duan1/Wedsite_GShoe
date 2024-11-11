12e01d1ca90127c9ee7b5ef142d5a1f122c1e89c
Ngọc Dũng <Phungdung2708@gmail.com>
2024-11-11 15:36:08 +0700

    Dung

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 header("Location: http://localhost/Wedsite_GShoe/Front-End/index.php");
 // Chuyển hướng sang trang index.php của Front-End/index.php
+// http://localhost/Wedsite_GShoe/Back-End/api/get_data.php đường dẫn api 
