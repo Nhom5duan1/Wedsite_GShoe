7fd4dbb94a23ed2bd03ccacfa7f14802fda3e4b0
Ngọc Dũng <Phungdung2708@gmail.com>
2024-11-11 13:09:56 +0700

    dung_commit-code

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 header("Location: http://localhost/Wedsite_GShoe/Front-End/index.php");
-// Chuyển hướng sang trang index.php của Front-End/index
+// Chuyển hướng sang trang index.php của Front-End/index.php
